17ef5f93a3fdb35f55f07bcfa5e727605d670301
Ralph Farinella <rfarinella@pushtechnology.com>
2013-03-04 22:51:19 -0500

    Added Comments

--- MyPublisher/src/com/pushtechnology/publisher/MyPublisher.java
+++ MyPublisher/src/com/pushtechnology/publisher/MyPublisher.java
@@ -21,11 +21,13 @@ public class MyPublisher extends Publisher {
 	   private Topic  EchoTopic= null;
 	   private static SingleValueTopicData TimeTopicData=null;
 
+	   //Runnable task to update Time Topic
 		static class ScheduledTask extends TimerTask {
 			public void run() {
 				Date date = new Date();
 
 				try {
+					//Simply update the data and tell diffusion to publish to all subscribers
 					TimeTopicData.updateAndPublish(date.toString());
 				} catch (TimeoutException e) {
 					// TODO Auto-generated catch block
@@ -42,9 +44,17 @@ public class MyPublisher extends Publisher {
 	    protected void initialLoad() throws APIException {
 			Date date = new Date();
 			System.out.println(date.toString());
+			
+			//Create Single Value Topic Data as a string
 			TimeTopicData = TopicDataFactory.newSingleValueData(MDataType.STRING);
+			
+			//Set Initial Value of Topic Data
 			TimeTopicData.initialise(date.toString());
+			
+			//Add Topic and Data to diffusion 
 			TimeTopic=addTopic("TIME",TimeTopicData);
+			
+			//Add additional Topic to support ECHO functionality
 			EchoTopic=addTopic("ECHO");
 
 			Timer time = new Timer();
